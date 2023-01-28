45afa209695bec45160a37047c5885f61736c732
crashonthebeat <64339619+crashonthebeat@users.noreply.github.com>
2023-01-28 15:56:12 -0800

    Added functionality for items and containers
    Added two args for Box __init__ to determine if box is closed and
    locked. Also added a list description for all boxes to display
    when getting, putting, or listing items. Added opening and closing
    of boxes, and then added the below-mentioned list items to the box
    description.
    
    Added the list items method to greatly improve item and room
    descriptions. The method will search itself for all items, and
    then search all open boxes recursively and display those items
    in their own strings.
    
    Added search_scope for a box itself so that the search scope can
    be called now on a scope or just a box.

--- entities/boxes.py
+++ entities/boxes.py
@@ -1,13 +1,24 @@
 from entities.basic import Item
+from src.text import numstr, proper, wraptxt
 
 class Box(Item):
     # Boxes are anything that has an internal inventory. A player and a room
     # can both be a box, as they both have inventories. Boxes can also 
     # contain other inventories.
-    def __init__(self, name):
-        Item.__init__(self, name)
+    def __init__(self, name, closed, locked):
+        Item.__init__(self, name, pl_name=False)
         self.inventory = {}
         self.isbox = True
+        self.closed = closed
+        self.locked = locked
+        self.list_desc = f"inside {self.name}"
+
+    def describe(self):
+        for line in self.desc: print(line)
+        if self.closed: pass
+        else: 
+            print(f"Inside {self.name}:")
+            [print(line) for line in self.list_items()]
 
     def add_item(self, item):
         if item in self.inventory.keys():
@@ -21,8 +32,90 @@ class Box(Item):
         else:
             self.inventory[item] -= 1
 
+    def search_scope(self, search_item):
+        found = 0
+        found_item = False
+
+        for item in self.inventory.keys():
+            if search_item in item.name and found == 0:
+                found += 1
+                found_item = item
+            elif search_item in item.name and found > 0:
+                found_item = 'multiple'
+
+        return found_item, self
+
+
+    def list_items(self):
+        # This method returns a list of strings for all items in the box,
+        # returning the plural name if the quantity is more than one.
+        # The method takes each open box and attempts to print the items 
+        # inside on one line, using textwrap to wrap long lines. 
+
+        openboxes = list()  # A blank list of all open boxes in the scope
+        nonboxes = dict()  # A blank list of all items that aren't open boxes
+
+        for item in self.inventory.keys():
+            # First all items are separated into the above categories
+            if (item.isbox and len(item.inventory) > 0) and not item.closed:
+                openboxes.append(item)
+            else:
+                nonboxes[item] = self.inventory[item]
+
+        itemcnt = len(nonboxes)  # A count of items left to add to the string
+        all_items = f"{proper(self.list_desc)}: "  # The itemlist starter
+
+        for item in nonboxes.keys():
+            qty = nonboxes[item]
+            if qty > 1 and itemcnt > 1:
+                # If there are items left and the item quantity is more than 1
+                # list the quantity of the item, the item, and add a comma.
+                all_items += f"{numstr(qty)} {item.pl_name}, "
+                itemcnt -= 1
+            elif qty > 1 and len(nonboxes) == 1:
+                # If there's just one item in the list, list only item and qty.
+                all_items += f"{numstr(qty)} {item.pl_name}."
+                itemcnt -= 1
+            elif qty > 1:
+                # Otherwise, end the list by starting with and.
+                all_items += f"and {numstr(qty)} {item.pl_name}."
+            elif qty == 1 and itemcnt > 1:
+                # Same as above 3, but only list the singular item name
+                all_items += f"{item.name}, "
+                itemcnt -= 1
+            elif qty == 1 and len(nonboxes) == 1:
+                all_items += f"{item.name}."
+                itemcnt -= 1
+            elif qty == 1:
+                all_items += f"and {item.name}."
+
+        itemlist = list()  # A blank list for all items in all boxes in scope
+        itemlist.extend(wraptxt(all_items))  # Start itemlist with root items
+            
+        for box in openboxes:  
+            # Add any open boxes to the list with recursive call
+            [itemlist.append(line) for line in box.list_items()]
+
+        return itemlist
+            
+    def open_box(self):
+        if self.closed and not self.locked:
+            self.closed = False
+            print(f"You open {self.name}.")
+        elif self.closed and self.locked:  # TODO implement door/box locks
+            print(f"{self.name.capitalize()} is locked.")
+        else:
+            print(f"{self.name.capitalize()} is already open")
+
+    def close_box(self):
+        if self.closed:
+            print(f"{self.name.capitalize()} is already closed.")
+        else:
+            self.closed = True
+            print(f"You close {self.name}.")
+
+
 class Holster(Box):
     # Holsters are boxes that have a specific slot for one type of item. 
      def __init__(self, name):
         Box.__init__(self, name)
-
