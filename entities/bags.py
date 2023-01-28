55c7997d8fad37282287fdc464aa982ec24b828b
crashonthebeat <64339619+crashonthebeat@users.noreply.github.com>
2023-01-28 15:54:03 -0800

    Added a Backpack class

---
+++ entities/bags.py
@@ -0,0 +1,14 @@
+# A bag is both an apparel and a box, like a backpack or pouch
+# All containers share layer 6
+
+from entities.boxes import Box
+from entities.items import Apparel
+
+class Backpack(Box, Apparel):
+    def __init__(self, name, pl_name, primary_slot='back'):
+        Apparel.__init__(self, name, pl_name, primary_slot='back')
+        self.primary_slot = primary_slot
+        self.inventory = {}
+        self.occupied_slots = {
+            'back': 6, 'l_shldr': 6, 'r_shldr': 6, 
+        }
